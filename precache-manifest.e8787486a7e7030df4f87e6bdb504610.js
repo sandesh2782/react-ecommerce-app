e36dec6b258a711064c1c3c851ce7b2272e2d3c5
sandesh2782 <49634982+sandesh2782@users.noreply.github.com>
2019-07-26 15:38:37 +0530

    Add files via upload
    Upload static site for react e-commerce app.

---
+++ precache-manifest.e8787486a7e7030df4f87e6bdb504610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c8fe27ab7895c43602ea54e0804f56",
+    "url": "/react-ecommerce-app/index.html"
+  },
+  {
+    "revision": "d8ab015bc2a620effbeb",
+    "url": "/react-ecommerce-app/static/css/main.30263412.chunk.css"
+  },
+  {
+    "revision": "8c45c639a9b2ca28ae63",
+    "url": "/react-ecommerce-app/static/js/2.314a555c.chunk.js"
+  },
+  {
+    "revision": "d8ab015bc2a620effbeb",
+    "url": "/react-ecommerce-app/static/js/main.78b3a038.chunk.js"
+  },
+  {
+    "revision": "35d0dc8ee6be8db61ecf",
+    "url": "/react-ecommerce-app/static/js/runtime~main.b0262846.js"
+  }
+]);
